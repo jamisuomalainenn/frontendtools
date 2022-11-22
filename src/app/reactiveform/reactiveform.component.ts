6bc5fbbc0e83ee247209c684dfb9077e5b8ee812
jamisuomalainenn <Jami.Suomalainen@student.lab.fi>
2022-11-22 17:00:17 +0200

    22.11

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -22,9 +22,9 @@ export class ReactiveformComponent implements OnInit {
       firstName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-Z]*$')]),
       lastName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-Z]*$')]),
       personid: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern("^(0[1-9]|[12]\\d|3[01])(0[1-9]|1[0-2])([5-9]\\d\+|\\d\\d-|[01]\\dA)\\d{3}[\\dA-Z]$")]),
-      email: new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z@0-9. ]*$")]),
+      email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
       username: new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern("^[a-zA-Z0-9 ]*$")]),
-      password: new FormControl('', [Validators.required, Validators.minLength(10), Validators.pattern("(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}$")]),
+      password: new FormControl('', [Validators.required, Validators.minLength(10), Validators.pattern("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{10,}$")]),
       password2: new FormControl('', [Validators.required, Validators.minLength(10), Validators.pattern("{{password.value}}")]),
       checkbox: new FormControl('', [Validators.requiredTrue])
     })
